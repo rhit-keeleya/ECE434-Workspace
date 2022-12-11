854ceb4ccf43efc4ba94b329db8bda19222ec057
Abel Keeley <keeleya@rose-hulman.edu>
2022-12-11 05:11:49 +0000

    Added switch2led script

---
+++ hw02/switch2led.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import gpiod
+import time
+import signal
+
+ledPins = [30,60,31,50]
+ledLines = []
+switchPins = [48,3,5,4]
+switchLines = []
+chips = []
+
+def handler(signum, frame):
+    print("\nClosing chips...")
+    for i in range(len(chips)):
+        chips[i].close()
+    exit(1)
+
+signal.signal(signal.SIGINT, handler)
+
+for pin in ledPins:
+    chipStr = "gpiochip"+str(int(pin/32))
+    lineNum = pin%32
+    chip = gpiod.Chip(chipStr)
+    line = chip.get_line(lineNum)
+    line.request(consumer="Blink", type=gpiod.LINE_REQ_DIR_OUT, default_val=0)
+    ledLines.append(line)
+    chips.append(chip)
+
+for pin in switchPins:
+    chipStr = "gpiochip"+str(int(pin/32))
+    lineNum = pin%32
+    chip = gpiod.Chip(chipStr)
+    line = chip.get_line(lineNum)
+    line.request(consumer="Blink", type=gpiod.LINE_REQ_DIR_IN, flags=gpiod.LINE_REQ_FLAG_BIAS_PULL_UP)
+    switchLines.append(line)
+    chips.append(chip)
+
+while True:
+    for i in range(len(ledPins)):
+        ledLines[i].set_value(not switchLines[i].get_value())
+    time.sleep(0.05)
